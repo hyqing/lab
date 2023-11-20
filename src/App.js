9423b9b722d34f4e98694f903370a01d2df5e700
hyq07508 <hyq07508@ly.com>
2023-11-20 15:57:56 +0800

    引入antd的样式

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Steps } from 'antd';
+import 'antd/dist/antd.css'
 import './App.css';
 
 function App() {
+  const description = 'This is a description.';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Steps
+          current={1}
+          items={[
+            {
+              title: 'Finished',
+              description,
+            },
+            {
+              title: 'In Progress',
+              description,
+              subTitle: 'Left 00:00:08',
+            },
+            {
+              title: 'Waiting',
+              description,
+            },
+          ]}
+      />
     </div>
   );
 }
